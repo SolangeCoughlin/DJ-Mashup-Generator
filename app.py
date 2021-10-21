f2b076ccde7a9c06cadccba994081ee5d93dd4bf
SolangeCoughlin <71780399+SolangeCoughlin@users.noreply.github.com>
2021-10-21 17:04:56 -0600

    HTML templates added, Navbar added

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# Starting here! 2 Test push
+from flask import Flask, render_template
+import os
+
+# Configuration
+ 
+app = Flask(__name__)
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template('home.html')
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
