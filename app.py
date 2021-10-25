b713dbf7cc50b906ea991e17313d539459fd4e02
SolangeCoughlin <71780399+SolangeCoughlin@users.noreply.github.com>
2021-10-25 17:35:56 -0600

    refactored showing song data and added more songs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import os, json
+import os, json, random
 
 # Configuration
  
@@ -17,15 +17,19 @@ def get_dict_from_json(json_file):
 
 @app.route('/')
 def root():
-    song_dict = get_dict_from_json('grail_song.json')
-    song_info_keys = song_dict.keys()
-    return render_template('home.html', keys = song_info_keys, data = song_dict)
+    
+    this_song_dict = get_dict_from_json('song_dict.json')["songs"]
+    index = random.randrange(0, len(this_song_dict))
+    song_info_keys = this_song_dict[index].keys()
+    return render_template('home.html', keys = song_info_keys, data = this_song_dict[index])
+
+@app.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')
 
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
-    #                                 ^^^^
-    #              You can replace this number with any valid port
+    port = int(os.environ.get('PORT', 9112))
     
     app.run(port=port, debug=True) 
